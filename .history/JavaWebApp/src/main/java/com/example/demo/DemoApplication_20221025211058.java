18027c5c98d673f8223ffaf2981834de60a1cabb
stephen <stephenmbianda@gmail.com>
2022-10-25 21:23:06 -0400

    fixes

--- .history/JavaWebApp/src/main/java/com/example/demo/DemoApplication_20221025211058.java
+++ .history/JavaWebApp/src/main/java/com/example/demo/DemoApplication_20221025211058.java
@@ -1,11 +1,17 @@
 package com.example.demo;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class DemoApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
-        String name = "vamsi";
-        System.out.println(name);
-    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+
+		string name = "steph";
+
+		system.out.println(name);
+	}
+
 }
